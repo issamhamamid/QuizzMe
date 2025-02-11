d3bf79597962c2ccfb6855eabdd4eaa6da70a15d
Issam Hammamid <issamhamamid6@gmail.com>
2025-02-11 05:22:17 +0100

    project structure and starting to work with typeorm

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+      TypeOrmModule.forRootAsync({
+        useFactory : ()=>({
+          type: 'mysql',
+          host: 'localhost',
+          port: 3306,
+          username: 'root',
+          password: 'root',
+          database: 'test',
+          autoLoadEntities : true ,
+          synchronize: true,
+        })
+      }),
+      UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/entities/user.entity.ts
@@ -0,0 +1,29 @@
+import {Column, Entity, PrimaryGeneratedColumn, Unique} from "typeorm";
+import {UserRole} from "../types/UserRole";
+
+@Entity()
+@Unique(['email , username'])
+
+export class User {
+    @PrimaryGeneratedColumn()
+    id : number
+
+    @Column()
+
+    full_name : string
+
+    @Column()
+    username : string
+
+    @Column()
+    email : string
+
+    @Column(
+        {
+            type: "enum",
+            enum: UserRole,
+            default: UserRole.USER,
+        }
+    )
+    role : UserRole
+}
