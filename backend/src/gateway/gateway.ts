0134f57e96a160a4146c9bbb08a43cdb29e6d93b
Issam Hammamid <issamhamamid6@gmail.com>
2025-02-15 20:38:23 +0100

    join and leave game room logic

--- backend/src/gateway/gateway.ts
+++ backend/src/gateway/gateway.ts
@@ -16,9 +16,30 @@ export class RoomGateWay implements OnModuleInit{
     }
 
     @SubscribeMessage('join-room')
-    joinRoom(@ConnectedSocket() client  : Socket , @MessageBody() data : any ){
-            client.join(data.room)
+    async joinRoom(@ConnectedSocket() client  : Socket , @MessageBody() data : any ){
+            Array.from(client.rooms).forEach((room)=>{
+                if(room!== client.id){
+                    client.leave(room)
+                }
+
+            })
+            await client.join(data.room)
             this.server.to(data.room).emit("joining" , `${data.username} joined the room`)
+    }
+
+    @SubscribeMessage('leave-room')
+    async leaveRoom(@ConnectedSocket() client : Socket , @MessageBody() username :string){
+        let curRoom: string = "";
+
+        for (const room of Array.from(client.rooms)) {
+            if (room !== client.id) {
+                await client.leave(room);
+                curRoom = room;
+            }
+        }
+
+        console.log(curRoom);
+        this.server.to(curRoom).emit("leaving" , `${username} left the room`)
     }
 
 }
