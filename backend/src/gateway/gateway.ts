a504e813dfc547bb5b8621e413439ec25e73820b
Issam Hammamid <issamhamamid6@gmail.com>
2025-02-17 17:24:08 +0100

    players scores init ad updating them each round

--- backend/src/gateway/gateway.ts
+++ backend/src/gateway/gateway.ts
@@ -1,11 +1,10 @@
 import {ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer} from "@nestjs/websockets";
-import {Inject, OnModuleInit, UseInterceptors} from "@nestjs/common";
+import {Inject, OnModuleInit} from "@nestjs/common";
 import  {Server , Socket} from 'socket.io'
 import {QuestionsService} from "../questions/questions.service";
 import {Question} from "../entities/question.entity";
 import Redis from "ioredis";
 import {getRoom} from "../util/getRoom";
-import {use} from "passport";
 
 
 
@@ -13,9 +12,11 @@ import {use} from "passport";
 export class RoomGateWay implements OnModuleInit{
 
 
+    private QUESTION_TIME =15;
+    private ROUND_TIME = 20;
 
 
-    constructor( @Inject('REDIS_CLIENT') private readonly redisClient: Redis ,  private questionsService : QuestionsService) {
+    constructor( @Inject('REDIS_CLIENT') private readonly redisClient: Redis ,  private questionsService : QuestionsService ) {
 
     }
 
@@ -61,62 +62,70 @@ export class RoomGateWay implements OnModuleInit{
 
 
         if (curRoom) {
+
             const questions: Question[] = await this.questionsService.generateRoomQuestion();
+            console.log(questions[0].answer)
             if (questions.length === 0) return; // Prevent errors if no questions
             const roomData = {
                 'created_at' : Date.now(),
                 'questions' : JSON.stringify(questions),
-                'isActive' : true
+                'isActive' : true,
+                'current_question' : JSON.stringify(questions[0])
             }
             await this.redisClient.hset(`room:${curRoom}`, roomData);
+            await this.initScores(curRoom)
+
             let currentQuestionIndex = 0;
-            let questionTimer = 3;
-            let roundTimer = 15;
+            let questionTimer = this.QUESTION_TIME;
+            let roundTimer = this.ROUND_TIME;
             let interval: NodeJS.Timeout;
 
             this.server.to(curRoom).emit("currentQuestion", questions[currentQuestionIndex]);
             let leaderboardShown = false
 
+            let isProcessing = false;  // Flag to check if the game loop is already processing an iteration
+
             const gameLoop = async () => {
+                if (isProcessing) return;  // If an iteration is already in progress, skip this one
 
+                isProcessing = true;  // Set flag to indicate processing has started
 
-
-
-                if( !leaderboardShown && (questionTimer === 0 || await this.didEveryoneSubmit(curRoom))){
-
-                    this.server.to(curRoom).emit("leaderboard", 'dd');
-                     this.redisClient.del('room1:answers')
-                    // TODO  : create leaderboard from answers and emit it
-                    leaderboardShown = true
-
+                if (!leaderboardShown && (questionTimer === 0 || await this.didEveryoneSubmit(curRoom))) {
+                    const new_scores = await this.updateScores(curRoom);
+                    this.server.to(curRoom).emit("leaderboard", new_scores);
+                    this.redisClient.del('room1:answers');
+                    // TODO: create leaderboard from answers and emit it
+                    leaderboardShown = true;
                 }
 
                 if (questionTimer > 0) {
                     this.server.to(curRoom).emit("timer", questionTimer);
                     questionTimer--;
-
                 }
 
                 if (roundTimer === 0) {
                     currentQuestionIndex++;
+
                     if (currentQuestionIndex >= questions.length) {
                         clearInterval(interval);
+                        isProcessing = false;  // Reset flag when game loop is stopped
                         return;
                     }
 
-
                     this.server.to(curRoom).emit("currentQuestion", questions[currentQuestionIndex]);
-                    roundTimer = 16;
-                    questionTimer = 3; // Reset timer for the next question
-                    leaderboardShown = false
-
+                    await this.redisClient.hset(`room:${curRoom}` , 'current_question' ,JSON.stringify(questions[currentQuestionIndex]) )
+                    roundTimer = this.ROUND_TIME + 1;
+                    questionTimer = this.QUESTION_TIME; // Reset timer for the next question
+                    leaderboardShown = false;
                 }
 
-
                 roundTimer--;
+
+                isProcessing = false;  // Reset flag after processing the current iteration
             };
 
             interval = setInterval(gameLoop, 1000);
+
         }
     }
 
@@ -139,6 +148,28 @@ export class RoomGateWay implements OnModuleInit{
         return submittedArray.length == roomPlayers.length
 
     }
+
+    async initScores(curRoom : string){
+        const roomPlayers = await  this.redisClient.smembers(`${curRoom}:players`)
+        for (const player of roomPlayers) {
+            await this.redisClient.hset(`${curRoom}:scores` ,`${player}`, 0)
+        }
+    }
+
+    async updateScores (curRoom : string) {
+
+        const usersAnswers = await this.redisClient.hgetall(`${curRoom}:answers`)
+        const current_question  = await this.redisClient.hget(`room:${curRoom}` , 'current_question')
+        const parsedQuestion : Question  = current_question ? JSON.parse(current_question) : null;
+        for(let user in usersAnswers){
+            if(this.questionsService.verifyAnswer(usersAnswers[user] , parsedQuestion)){
+                await this.redisClient.hincrby(`${curRoom}:scores` ,`${user}` , 10)
+            }
+        }
+        return this.redisClient.hgetall(`${curRoom}:scores`);
+
+    }
+
 
 
 
