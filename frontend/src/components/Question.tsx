970c6b6ab5993e44a6472e2ff4461ffe5a2210ee
Issam Hammamid <issamhamamid6@gmail.com>
2025-02-26 19:10:21 +0100

    question component corrct answer ui

--- frontend/src/components/Question.tsx
+++ frontend/src/components/Question.tsx
@@ -1,6 +1,6 @@
 
 import { HiUsers } from "react-icons/hi";
-
+import { FaCheck } from "react-icons/fa6";
 
 export const Question = () => {
     return (
@@ -32,26 +32,33 @@ export const Question = () => {
                 <h1 className='text-3xl text-white font-bold mb-10  text-center'>In which Hawaiian city was Barack Obama
                     born?</h1>
 
-                <div className=' flex flex-col w-full xl:w-9/10 xl:grid xl:grid-cols-2 gap-5 '>
-                    <button className=' cursor-pointer bg-gray-200 pb-2 rounded-2xl'>
+                <div className=' flex flex-col w-full xl:w-9/10 xl:grid xl:grid-cols-2 gap-5 relative '>
+
+
+                    <button className=' cursor-pointer bg-gray-200 pb-2 rounded-2xl relative'>
+                        <div className='bg-white h-11 w-11 rounded-full absolute -right-5 -top-3 flex items-center justify-center '>
+                            <FaCheck className='text-[1.5rem]  '/>
+
+                        </div>
                         <div className='py-5 bg-[#FCA5A5] rounded-2xl border-3 border-white shadow-2xl '>
                             <p className='text-center text-[#872525] text-2xl '>Makawao</p>
                         </div>
                     </button>
 
-                    <button className=' cursor-pointer bg-gray-200 pb-2 rounded-2xl'>
-                        <div className='py-5 bg-[#93C5FD] rounded-2xl border-3 border-white shadow-2xl '>
+                    <button className='opacity-55 cursor-pointer bg-gray-200 pb-2 rounded-2xl'>
+                    <div className='py-5 bg-[#93C5FD] rounded-2xl border-3 border-white shadow-2xl '>
                             <p className='text-center text-blue-900 text-2xl '>Lahaina</p>
                         </div>
                     </button>
 
-                    <button className=' cursor-pointer bg-gray-200 pb-2 rounded-2xl'>
+                    <button className=' opacity-55  cursor-pointer bg-gray-200 pb-2 rounded-2xl'>
                         <div className='py-5 bg-yellow-300 rounded-2xl border-3 border-white shadow-2xl '>
                             <p className='text-center text-[#872525] text-2xl '>Puhi</p>
                         </div>
                     </button>
 
-                    <button className=' cursor-pointer bg-gray-200 pb-2 rounded-2xl'>
+                    <button className=' opacity-55 cursor-pointer bg-gray-200 pb-2 rounded-2xl relative'>
+
                         <div className='py-5 bg-green-300 rounded-2xl border-3 border-white shadow-2xl '>
                             <p className='text-center text-green-900 text-2xl '>DDY</p>
                         </div>
